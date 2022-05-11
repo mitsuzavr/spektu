09c1d6f583c418136f9b906f983cb12c5de79462
mitsuzavr <michael.shelenberg@protonmail.com>
2022-05-11 16:12:51 +0200

    :construction: Add bookmarks

--- src/scripts/marker.js
+++ src/scripts/marker.js
@@ -11,6 +11,7 @@ const percentColors = [
   { pct: 0.5, color: { r: 255, g: 165, b: 0 } },
   { pct: 1.0, color: { r: 0, g: 200, b: 0 } },
 ];
+
 function getColorForPercentage(pct, a) {
   for (var i = 1; i < percentColors.length - 1; i++) {
     if (pct < percentColors[i].pct) {
@@ -28,26 +29,31 @@ function getColorForPercentage(pct, a) {
     g: Math.floor(lower.color.g * pctLower + upper.color.g * pctUpper),
     b: Math.floor(lower.color.b * pctLower + upper.color.b * pctUpper),
   };
+
   return "rgba(" + [color.r, color.g, color.b, a].join(",") + ")";
 }
 
-// Progress
 chrome.storage.sync.get(["key"], (result) => {
   let savedVideos = JSON.parse(result.key || "[]");
+
   let menuEl = document.querySelector("div#vMenu");
   menuEl.querySelectorAll("a.hasSubmenu").forEach((plEl) => {
     let videosCompleted = 0; // Counts how many videos have been completed
 
-    // Videos progress
     let videoEls = plEl.nextElementSibling.querySelectorAll("a.edittllect");
     videoEls.forEach((vEl) => {
+      vEl.style.width = "100%";
+      vEl.style.margin = "0";
+
+      // ! Bookmark
+      let courseEl = plEl.closest("div#vMenu > ul");
+      let courseTitle = courseEl.querySelector("a").textContent;
+
+      // Video progress
       let vUrl = vEl.getAttribute("href");
       let savedVideo = savedVideos.find((sV) => sV.url === vUrl);
 
       if (!savedVideo) return; // Showing video progress is skipped, if the video is not in localStorage
-
-      vEl.style.width = "100%";
-      vEl.style.margin = "0";
 
       let progressEl = document.createElement("span");
       progressEl.style.float = "right";
@@ -57,10 +63,12 @@ chrome.storage.sync.get(["key"], (result) => {
           <!-- Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
           <path d="M438.6 105.4C451.1 117.9 451.1 138.1 438.6 150.6L182.6 406.6C170.1 419.1 149.9 419.1 137.4 406.6L9.372 278.6C-3.124 266.1-3.124 245.9 9.372 233.4C21.87 220.9 42.13 220.9 54.63 233.4L159.1 338.7L393.4 105.4C405.9 92.88 426.1 92.88 438.6 105.4H438.6z"/>
         </svg>`;
+
         videosCompleted++;
       } else {
         let percentage = savedVideo.progress / savedVideo.duration;
         let offset = circumference - percentage * circumference;
+
         progressEl.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="24" height="24">
           <circle
@@ -80,6 +88,7 @@ chrome.storage.sync.get(["key"], (result) => {
     // Playlist progress
     let percentage = videosCompleted / videoEls.length;
     let bgWidth = Math.round(percentage * 100);
+
     plEl.style.background = `
     linear-gradient(
       to right,
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,9 +1,45 @@
-// document.getElementById("deleteButton").addEventListener("click", () => {
-//   document.getElementById("bookmark").style.display = "none";
-// });
+// -- Bookmarks --
+function onRejected(error) {
+  console.log(`An error: ${error}`);
+}
 
-console.log("I'm alive!");
+function findFolderID(folderTitle, subTreeID) {
+  chrome.bookmarks.getSubTree(subTreeID).then((subTree) => {
+    let subTreeFolder = subTree[0];
+    let foundFolder = false;
 
+    subTreeFolder.children.forEach((bookmarkItem) => {
+      if (!bookmarkItem.url)
+        if (bookmarkItem.title === folderTitle) {
+          folderID = bookmarkItem.id;
+          foundFolder = true;
+          console.log(
+            `Found folder ${bookmarkItem.title} with ID ${bookmarkItem.id}`
+          );
+        }
+    });
+
+    if (!foundFolder) {
+      chrome.bookmarks.create(
+        { parentId: subTreeID, title: folderTitle },
+        (newFolder) => {
+          folderID = newFolder.id;
+          console.log(
+            `Created folder ${newFolder.title} with ID ${newFolder.id}`
+          );
+        }
+      );
+    }
+  }, onRejected);
+}
+
+// ! listBookmarks();
+
+// ! removeBookmark();
+
+var folderID = findFolderID("Spektu bookmarks", "2"); // 2 is Other bookmarks folder ID
+
+// -- Options --
 document.querySelector("#options-btn").addEventListener("click", () => {
   if (chrome.runtime.openOptionsPage) chrome.runtime.openOptionsPage();
   else window.open(chrome.runtime.getURL("options.html"));
